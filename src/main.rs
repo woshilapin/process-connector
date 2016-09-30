0333734039a495f6f7a632837f7a6113c55feb9f
Jean SIMARD <woshilapin@tuziwo.info>
2016-09-30 19:30:10 -0400

    Fix blocking read on input

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,30 @@
 extern crate getopts;
 
 use std::env;
-use std::io::{BufRead, BufReader, BufWriter, Write};
-use std::process::{Child, Command, Stdio};
 use getopts::Options;
+use process::Process;
+mod process;
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} [options] <botA_cmd> <botB_cmd>", program);
     print!("{}", opts.usage(&brief));
 }
-fn spawn_from_cmd(cmd: String) -> Child {
-    let cmd_array: Vec<&str> = cmd.split(" ").collect();
-    let child_result = Command::new(cmd_array[0])
-        .args(&cmd_array[1..])
-        .stdout(Stdio::piped())
-        .stdin(Stdio::piped())
-        .spawn();
-    match child_result {
-        Ok(child) => child,
-        Err(e) => panic!("error: bot a: {}", e),
-    }
-}
 
-fn connect(bot_a_child: Child, bot_b_child: Child) {
-    let bot_a_stdin = match bot_a_child.stdin {
-        Some(value) => value,
-        None => panic!("error: bot a: cannot access stdin"),
-    };
-    let bot_a_stdout = match bot_a_child.stdout {
-        Some(value) => value,
-        None => panic!("error: bot a: cannot access stdout"),
-    };
-    let bot_b_stdin = match bot_b_child.stdin {
-        Some(value) => value,
-        None => panic!("error: bot b: cannot access stdin"),
-    };
-    let bot_b_stdout = match bot_b_child.stdout {
-        Some(value) => value,
-        None => panic!("error: bot b: cannot access stdout"),
-    };
-    let mut bot_a_reader = BufReader::new(bot_a_stdout);
-    let mut bot_a_writer = BufWriter::new(bot_a_stdin);
-    let mut bot_b_writer = BufWriter::new(bot_b_stdin);
-    let mut bot_b_reader = BufReader::new(bot_b_stdout);
-    
+fn connect(process_a: &mut Process, process_b: &mut Process) {
     loop {
-        let mut stdout = std::io::stdout();
-        let mut bot_a_line = String::new();
-        if bot_a_reader.read_line(&mut bot_a_line).unwrap() > 0 {
-            bot_b_writer.write_fmt(format_args!("{}", bot_a_line)).unwrap();
-            bot_b_writer.flush().unwrap();
-            stdout.write_fmt(format_args!("bot a: {}", bot_a_line)).unwrap();
-            stdout.flush().unwrap();
+        match process_a.pop() {
+            Some(packet) => {
+                println!("{}", packet);
+                process_b.push(packet);
+            }
+            None => {}
         }
-        let mut bot_b_line = String::new();
-        if bot_b_reader.read_line(&mut bot_b_line).unwrap() > 0 {
-            bot_a_writer.write_fmt(format_args!("{}", bot_b_line)).unwrap();
-            bot_a_writer.flush().unwrap();
-            stdout.write_fmt(format_args!("bot b: {}", bot_b_line)).unwrap();
-            stdout.flush().unwrap();
+        match process_b.pop() {
+            Some(packet) => {
+                println!("{}", packet);
+                process_a.push(packet);
+            }
+            None => {}
         }
     }
 }
@@ -77,21 +43,22 @@ fn main() {
         print_usage(&program, opts);
         return;
     }
-    let bot_a_cmd = if !matches.free.is_empty() {
+    let cmd_a = if !matches.free.is_empty() {
         matches.free[0].clone()
     } else {
         print_usage(&program, opts);
         return;
     };
-    let bot_b_cmd = if matches.free.len() > 1 {
+    let cmd_b = if matches.free.len() > 1 {
         matches.free[1].clone()
     } else {
         print_usage(&program, opts);
         return;
     };
 
-    let bot_a_child = spawn_from_cmd(bot_a_cmd);
-    let bot_b_child = spawn_from_cmd(bot_b_cmd);
-
-    connect(bot_a_child, bot_b_child);
+    let mut process_a = Process::new("game", cmd_a);
+    let mut process_b = Process::new("bot", cmd_b);
+    process_a.init();
+    process_b.init();
+    connect(&mut process_a, &mut process_b);
 }
